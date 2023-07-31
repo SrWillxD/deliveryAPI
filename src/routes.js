c92872d036f78ed909be95f019d1eb197f4d1b38
SrWillxD <willgra09@hotmail.com>
2023-07-31 18:23:50 -0300

    Adding an endpoint to create an order

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import express from 'express';
 const routes = express.Router();
 
+import createAnOrder from "./middlewares/createAnOrder.js";
 
 
-
+routes.post("/criarpedido", async(req, res)=>{
+    try {
+        const bodyParams = req.body;
+        const result = await createAnOrder(bodyParams);
+        res.send(result);
+    }catch (err) {
+        console.error('Error in /criarpedido route:', err);
+        res.status(500).json({ error: 'Internal server error'});
+    }
+})
 
 
 
