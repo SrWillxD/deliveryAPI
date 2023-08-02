f7dc7a3e5970474058b685f69ce60ae77f64c9d7
SrWillxD <willgra09@hotmail.com>
2023-08-02 12:56:47 -0300

    Adding a route to update an order and a method that do the change

---
+++ src/middlewares/updateOrder.js
@@ -0,0 +1,37 @@
+import captureJSON from "./capture.js";
+import writeJSON from "./writeJSON.js"
+
+async function updateOrder(idOrder, bodyParams){
+    const filePath = "./pedidos.json";
+    const jsonData = await captureJSON(filePath);
+
+    const orderToBeUpdated = {};
+
+    jsonData.pedidos.forEach(element => {
+        if(element.id === idOrder){
+            orderToBeUpdated.index = element.id -1;
+            orderToBeUpdated.objToBeUpdated = element;
+        }
+    });
+
+    if (Object.keys(orderToBeUpdated).length === 0) {
+        return "O pedido informado não existe.";
+    }
+
+    orderToBeUpdated.objToBeUpdated = {
+        "id": bodyParams.id, 
+        "cliente": bodyParams.cliente,
+        "produto": bodyParams.produto,
+        "valor": bodyParams.valor,
+        "entregue": bodyParams.entregue,
+        "timestamp": bodyParams.timestamp
+    }
+
+    jsonData.pedidos[orderToBeUpdated.index] = orderToBeUpdated.objToBeUpdated;
+
+    writeJSON(filePath, jsonData);
+
+    return jsonData;
+}
+
+export default updateOrder;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 const routes = express.Router();
 
 import createAnOrder from "./middlewares/createAnOrder.js";
+import updateOrder from "./middlewares/updateOrder.js"
 
 
 routes.post("/criarpedido", async(req, res)=>{
@@ -13,7 +14,29 @@ routes.post("/criarpedido", async(req, res)=>{
         console.error('Error in /criarpedido route:', err);
         res.status(500).json({ error: 'Internal server error'});
     }
-})
+});
+
+routes.put("/atualizarpedido/:idOrder", async(req, res)=>{
+    try {
+        const idOrder = parseInt (req.params.idOrder);
+        const bodyParams = req.body;
+
+        if (isNaN(idOrder)){
+            return res.status(400).json('O parâmetro deve ser um número inteiro válido.');
+        }
+
+        const result = await updateOrder(idOrder, bodyParams);
+
+        if (result === "O pedido informado não existe.") {
+            return res.status(400).json(result);
+        }
+
+        res.send(result);
+    } catch(err){
+        console.error('Error in /atualizarpedido route:', err);
+        res.status(500).json({ error: 'Internal server error'});
+    }
+});
 
 
 
